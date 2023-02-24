79a2b8ed026c938630fb47f9099fde66c50bf0b0
Alicia <arrowsrp@yahoo.com>
2023-02-24 14:50:35 -0800

    Creates ConversationDashboard component and imports/renders in App.js.

---
+++ src/components/ConversationDashboard.js
@@ -0,0 +1,16 @@
+export default function ConversationDashboard () {
+    return (
+        <div>
+            <div>
+                <h3>Active Conversations</h3>
+            </div>
+            <div>
+                <h3>Participating Conversation</h3>
+            </div>
+            <div>
+                <h3>Your Conversations</h3>
+            </div>
+        </div>    
+
+    )
+}
