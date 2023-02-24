10e1cc3d6952128067a49dd9ce5c23a4fc58fc0e
Alicia <arrowsrp@yahoo.com>
2023-02-24 14:39:39 -0800

    Imports createBrowserRouter, and RouteProvider.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
+import Login from './components/Login';
 import reportWebVitals from './reportWebVitals';
 
 const router = createBrowserRouter([
